fbfbb87bd9a74085fe16fa0c9c4ebef2b2876459
JayJayCEO <jayjaylab.ceo@gmail.com>
2014-11-04 00:00:27 +0900

    finis robolectric configuration

--- app/src/androidTest/java/com/jayjaylab/androiddemo/ActivityIntroTest.java
+++ app/src/androidTest/java/com/jayjaylab/androiddemo/ActivityIntroTest.java
@@ -1,11 +1,22 @@
 package com.jayjaylab.androiddemo;
 
+import android.app.Activity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 import java.lang.Exception;
 
+import static junit.framework.Assert.assertTrue;
+
 @Config(manifest = "./src/main/AndroidManifest.xml")
-@RunWith(RobolectricRoboTestRunner.class)
-public class ActivityIntroTes {
+@RunWith(RobolectricTestRunner.class)
+public class ActivityIntroTest {
     @Test
     public void testActivityIsNotNull() throws Exception {
+        Activity activity = Robolectric.buildActivity(ActivityIntro.class).create().get();
+        assertTrue(activity != null);
     }
 }
