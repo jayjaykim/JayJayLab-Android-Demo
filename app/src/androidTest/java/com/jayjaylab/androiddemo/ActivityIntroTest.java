ed134b73f2dde1d77cbef15f5f80b693156e80c6
JayJayCEO <jayjaylab.ceo@gmail.com>
2014-11-06 00:03:38 +0900

    added before after annotation

--- app/src/androidTest/java/com/jayjaylab/androiddemo/ActivityIntroTest.java
+++ app/src/androidTest/java/com/jayjaylab/androiddemo/ActivityIntroTest.java
@@ -2,6 +2,8 @@ package com.jayjaylab.androiddemo;
 
 import android.app.Activity;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -9,11 +11,23 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import java.lang.Exception;
 
+import roboguice.RoboGuice;
+
 import static junit.framework.Assert.assertTrue;
 
 @Config(manifest = "./app/src/main/AndroidManifest.xml")
 @RunWith(RobolectricTestRunner.class)
 public class ActivityIntroTest {
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void teardown() {
+
+    }
+
     @Test
     public void testIfActivityIsNotNull() throws Exception {
         Activity activity = Robolectric.buildActivity(ActivityIntro.class).create().get();
