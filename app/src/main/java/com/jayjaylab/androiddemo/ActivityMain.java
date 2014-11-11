019767bd8bacec241a47fef37d957d94fd3bfc8b
JayJayCEO <jayjaylab.ceo@gmail.com>
2014-11-11 22:35:58 +0900

    add Toolbar and RecyclerView

--- app/src/main/java/com/jayjaylab/androiddemo/ActivityMain.java
+++ app/src/main/java/com/jayjaylab/androiddemo/ActivityMain.java
@@ -2,6 +2,8 @@ package com.jayjaylab.androiddemo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -9,19 +11,25 @@ import com.google.inject.Inject;
 import com.jayjaylab.androiddemo.R;
 import com.jayjaylab.androiddemo.view.ImageViewThreadPool;
 
+import roboguice.activity.RoboActionBarActivity;
 import roboguice.activity.RoboListActivity;
 import roboguice.activity.event.OnPauseEvent;
 import roboguice.context.event.OnCreateEvent;
 import roboguice.event.Observes;
 import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 import roboguice.util.Ln;
 
 @ContentView(R.layout.activity_main)
-public class ActivityMain extends RoboListActivity {
-    @Inject ImageViewThreadPool  imageViewThreadPool;
+public class ActivityMain extends RoboActionBarActivity {
+//    @Inject ImageViewThreadPool  imageViewThreadPool;
+
+    // views
+    @InjectView(R.id.toolbar) Toolbar toolbar;
+    @InjectView(R.id.recycler_view) RecyclerView recyclerView;
 
     public void onCreateEvent(@Observes OnCreateEvent event) {
-
+        setSupportActionBar(toolbar);
     }
 
     public void onPauseEvent(@Observes OnPauseEvent event) {
