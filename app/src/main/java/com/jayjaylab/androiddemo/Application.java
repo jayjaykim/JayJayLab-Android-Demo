056e3783cdfd356dbb066bc39bee1dfc7f48278d
JayJayCEO <jayjaylab.ceo@gmail.com>
2014-11-13 00:47:42 +0900

    apply ACRA

---
+++ app/src/main/java/com/jayjaylab/androiddemo/Application.java
@@ -0,0 +1,27 @@
+package com.jayjaylab.androiddemo;
+
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+/**
+ * Created by jongjoo on 11/13/14.
+ */
+@ReportsCrashes(
+        formKey = "",
+        mailTo = "jayjay@diogroup.co.kr",
+        customReportContent = {ReportField.APP_VERSION_CODE, ReportField.ANDROID_VERSION,
+                ReportField.APP_VERSION_NAME, ReportField.PHONE_MODEL, ReportField.STACK_TRACE,
+                ReportField.LOGCAT},
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_toast_text
+)
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ACRA.init(this);
+    }
+}
