3c30fbfc4357873ef5aeb5575cb790fee6229696
JayJayCEO <jayjaylab.ceo@gmail.com>
2014-11-13 23:42:58 +0900

    add layout for greyhound app

--- app/src/main/java/com/jayjaylab/androiddemo/app/greyhound/activity/ActivityMain.java
+++ app/src/main/java/com/jayjaylab/androiddemo/app/greyhound/activity/ActivityMain.java
@@ -7,24 +7,10 @@ import android.view.MenuItem;
 
 import com.jayjaylab.androiddemo.R;
 
-public class ActivityMain extends ActionBarActivity {
+import roboguice.activity.RoboActionBarActivity;
+import roboguice.inject.ContentView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_greyhound_main);
-    }
+@ContentView(R.layout.activity_greyhound_main)
+public class ActivityMain extends RoboActionBarActivity {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        return super.onOptionsItemSelected(item);
-    }
 }
