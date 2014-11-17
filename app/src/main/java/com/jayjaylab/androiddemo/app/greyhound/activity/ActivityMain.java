14e7ff3d2527577fc0c37375ba9b353b7d0746d6
JayJayCEO <jayjaylab.ceo@gmail.com>
2014-11-17 22:45:38 +0900

    add fragment for history

--- app/src/main/java/com/jayjaylab/androiddemo/app/greyhound/activity/ActivityMain.java
+++ app/src/main/java/com/jayjaylab/androiddemo/app/greyhound/activity/ActivityMain.java
@@ -1,12 +1,15 @@
 package com.jayjaylab.androiddemo.app.greyhound.activity;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.inject.Inject;
 import com.jayjaylab.androiddemo.R;
+import com.jayjaylab.androiddemo.app.greyhound.fragment.FragmentPathHistory;
 
 import roboguice.activity.RoboActionBarActivity;
 import roboguice.context.event.OnCreateEvent;
@@ -17,10 +20,17 @@ import roboguice.inject.InjectView;
 @ContentView(R.layout.activity_greyhound_main)
 public class ActivityMain extends RoboActionBarActivity {
 
+    @Inject FragmentPathHistory fragmentPathHistory;
     // views
     @InjectView(R.id.toolbar) Toolbar toolbar;
 
     public void onCreateEvent(@Observes OnCreateEvent event) {
         setSupportActionBar(toolbar);
+    }
+
+    protected void loadPathHistoryFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, fragmentPathHistory, fragmentPathHistory.TAG);
+        transaction.commit();
     }
 }
